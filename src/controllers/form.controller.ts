b68779bf88857dd97509481f9e62d7a19a7edf43
Akshay Priyadarshi <akshayp1904@outlook.com>
2024-06-18 23:25:32 +0530

    Fixed controller and service

--- src/controllers/form.controller.ts
+++ src/controllers/form.controller.ts
@@ -1,12 +1,79 @@
 import { Router, Request, Response, NextFunction } from "express"
 import { FormService } from "../services/form.service"
+import { ApiResponse } from "../models/response.model"
 
 export const FormController = Router()
 const formService = new FormService()
 
 FormController.get(
+    "/:formId?",
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const formReadRes = await formService.readForm(req.params.formId)
+            const apiResponse = new ApiResponse(
+                `Form${req.params.formId === undefined ? "s" : ""} read successfully!`,
+                200,
+                formReadRes
+            )
+            res.status(apiResponse.statusCode).json(apiResponse)
+        } catch (error) {
+            next(error)
+        }
+    }
+)
+
+FormController.post(
     "/",
     async (req: Request, res: Response, next: NextFunction) => {
-        res.status(200).json(await formService.readForm())
+        try {
+            const formCreateRes = await formService.createForm(req.body)
+            const apiResponse = new ApiResponse(
+                `Form created successfully!`,
+                200,
+                formCreateRes
+            )
+            res.status(apiResponse.statusCode).json(apiResponse)
+        } catch (error) {
+            next(error)
+        }
+    }
+)
+
+FormController.patch(
+    "/:formId",
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const formUpdateRes = await formService.updateForm(
+                req.params.formId,
+                req.body
+            )
+            const apiResponse = new ApiResponse(
+                `Form updated successfully!`,
+                200,
+                formUpdateRes
+            )
+            res.status(apiResponse.statusCode).json(apiResponse)
+        } catch (error) {
+            next(error)
+        }
+    }
+)
+
+FormController.delete(
+    "/:formId",
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const formDeleteRes = await formService.deleteForm(
+                req.params.formId
+            )
+            const apiResponse = new ApiResponse(
+                `Form deleted successfully!`,
+                200,
+                formDeleteRes
+            )
+            res.status(apiResponse.statusCode).json(apiResponse)
+        } catch (error) {
+            next(error)
+        }
     }
 )
--- src/services/form.service.ts
+++ src/services/form.service.ts
@@ -1,7 +1,45 @@
+import { Form } from "../models/form.model"
+import { IForm } from "../interfaces/form"
+import { BadRequestError } from "../errors/bad-request.error"
+import { isValidObjectId } from "mongoose"
+
 export class FormService {
-    readForm = async (formId?: string) => {
-        return {
-            forms: "Here are all the forms"
+    readForm = async (formId?: string): Promise<IForm | IForm[]> => {
+        let formReadRes: IForm | IForm[]
+        if (formId) {
+            if (isValidObjectId(formId) === false) {
+                throw new BadRequestError("Invalid form ID!")
+            }
+            const formReadResOrNull = await Form.findById(formId)
+            if (formReadResOrNull === null) {
+                throw new BadRequestError("Form not found!")
+            }
+            formReadRes = formReadResOrNull
+        } else {
+            formReadRes = await Form.find({})
         }
+        return formReadRes
+    }
+
+    createForm = async (createFormDto: IForm): Promise<IForm> => {
+        const formCreateRes = await Form.create(createFormDto)
+        return formCreateRes
+    }
+
+    updateForm = async (
+        formId: string,
+        updateFormDto: IForm
+    ): Promise<IForm | null> => {
+        const formUpdateRes = await Form.findByIdAndUpdate(
+            formId,
+            updateFormDto,
+            { new: true }
+        )
+        return formUpdateRes
+    }
+
+    deleteForm = async (formId: string): Promise<IForm | null> => {
+        const formUpdateRes = await Form.findByIdAndDelete(formId)
+        return formUpdateRes
     }
 }
